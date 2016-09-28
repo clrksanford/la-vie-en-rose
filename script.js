924dbea7d1bd01cc6fe9348a1a0a517d06f5762c
Clark Sanford <clrksanford@gmail.com>
2016-09-28 14:47:36 -0500

    Added color randomizing button

--- script.js
+++ script.js
@@ -37,12 +37,34 @@ $(document).ready(function() {
 // Reset function
 
 $(document).ready(function() {
-  $('button').click(function() {
+  $('.reset').click(function() {
     $('.jumbotron').css('background', "url('images/edith2.jpg')");
     $('.jumbotron').css('background-position','center');
     $('.jumbotron').css('background-size','cover');
     $('.filter').css('background-color','rgba(255,255,255,0)');
     $('h2').css('visibility','visible');
     $('select').prop('selectedIndex',0);
+  });
+});
+
+//Color randomizer
+function colorRandomizer () {
+  var red = Math.round(Math.random() * 255);
+  var green = Math.round(Math.random() * 255);
+  var blue = Math.round(Math.random() * 255);
+
+  var color = 'rgba(' + red + "," + green + "," + blue + ",0.25)";
+
+  return color;
+}
+
+$(document).ready(function() {
+  $('.random').click(function() {
+
+    var color = colorRandomizer();
+
+    $('.filter').css('background-color', color);
+    // $('header').css('background-color', 'rgba(0,244,0,0.2)');
+    // $('.image-selector').css('background-color', 'rgba(0,244,0,0.2)');
   });
 });
