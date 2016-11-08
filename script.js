64b0f291a283bf572b76827597bd7d38163c2381
Clark Sanford <clrksanford@gmail.com>
2016-11-08 16:22:33 -0600

    Random styling changes, trying to fix navbar

--- script.js
+++ script.js
@@ -33,16 +33,10 @@ function colorSelect () {
   var userColor = $('#color-picker :selected').text();
   if (userColor === "rose") {
     $('.filter').css('background-color', 'rgba(244,0,0,0.25)');
-    $('header').css('background-color', '#ffb3b3');
-    $('.image-selector').css('background-color', '#ff8080');
   } else if (userColor === "bleu") {
     $('.filter').css('background-color', 'rgba(0,0,244,0.25)');
-    $('header').css('background-color', 'rgba(0,0,244,0.2)');
-    $('.image-selector').css('background-color', 'rgba(0,0,244,0.2)');
   } else if (userColor === "vert") {
     $('.filter').css('background-color', 'rgba(0,244,0,0.25)');
-    $('header').css('background-color', 'rgba(0,244,0,0.2)');
-    $('.image-selector').css('background-color', 'rgba(0,244,0,0.2)');
   }
 }
 
@@ -52,7 +46,7 @@ function reset() {
   $('.jumbotron').css('background', "url('images/edith2.jpg')");
   $('.jumbotron').css('background-position','center');
   $('.jumbotron').css('background-size','cover');
-  $('.filter').css('background-color','rgba(255,255,255,0)');
+  $('.filter').css('background-color','rgba(255,0,0,0.3)');
   $('h2').css('visibility','visible');
   $('select').prop('selectedIndex',0);
 }
